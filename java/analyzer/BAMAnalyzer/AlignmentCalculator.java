dcf6d2660c7e941332ce009762177f6d9ec9208e
David Rio Deiros <driodeiros@gmail.com>
2011-09-19 10:10:45 -0500

    * Allowing multiple BAMs input * Adding new attribute in stats (EffectiveBases) to account for duplicates.

--- java/analyzer/BAMAnalyzer/AlignmentCalculator.java
+++ java/analyzer/BAMAnalyzer/AlignmentCalculator.java
@@ -23,21 +23,23 @@ public class AlignmentCalculator extends MetricsCalculator
   private long mappedReads       = 0;  // Number of mapped reads
   private long unmappedReads     = 0;  // Number of unmapped reads
   private long dupReads          = 0;  // Number of duplicate reads
-  
-  private long totalValidBases   = 0;  // Total number of bases excluding Ns
-  private long totalBases        = 0;  // Total number of bases including Ns
-  private long totalMappedBases  = 0;  // Number of bases for reads that map
-                                       // (Partially or completely)
-  private long totalMismatches   = 0;  // Total number of mismatches
-  private long totalExactMatches = 0;  // Total number of reads with no mismatches
-  
-  private double percentMapped     = 0;  // Percent of mapped reads
-  private double percentMismatch   = 0;  // Mismatch percentage (Error percentage)
-  private double percentDup        = 0;  // Percentage of duplicate reads
+
+  private long totalValidBases   = 0;    // Total number of bases excluding Ns
+  private long totalBases        = 0;    // Total number of bases including Ns
+  private long totalMappedBases  = 0;    // Number of bases for reads that map
+                                         // (Partially or completely)
+  private long totalEffectiveBases = 0;  // Bases that are not N's and are not from repeats
+
+  private long totalMismatches     = 0;  // Total number of mismatches
+  private long totalExactMatches   = 0;  // Total number of reads with no mismatches
+
+  private double percentMapped     = 0; // Percent of mapped reads
+  private double percentMismatch   = 0; // Mismatch percentage (Error percentage)
+  private double percentDup        = 0; // Percentage of duplicate reads
   private double percentExactMatch = 0; // Percentage of matching reads with no variation
-  
+
   private MismatchCounter mCtr  = null; // To count the number of mismatches
-  
+
   /**
    * Class constructor. Collect metrics only for the specified read type
    * @param rType
@@ -48,7 +50,7 @@ public class AlignmentCalculator extends MetricsCalculator
     this.readType = rType;
     mCtr = new MismatchCounter();
   }
-  
+
 
   /**
    * Method to process the next read
@@ -63,7 +65,7 @@ public class AlignmentCalculator extends MetricsCalculator
       computeAlignmentMetrics(nextRead);
     }
   }
-  
+
   /**
    * Calculate the results.
    */
@@ -73,7 +75,7 @@ public class AlignmentCalculator extends MetricsCalculator
     if(totalReads > 0)
     {
       percentMapped = 1.0 * mappedReads / totalReads * 100;
-	      
+
       if(mappedReads > 0)
       {
         percentDup = 1.0 * dupReads / mappedReads * 100.0;
@@ -84,7 +86,7 @@ public class AlignmentCalculator extends MetricsCalculator
         percentDup = 0;
         percentExactMatch = 0;
       }
-	      
+
       if(totalMappedBases > 0)
         percentMismatch = 1.0 * totalMismatches / totalMappedBases * 100.0;
       else
@@ -105,27 +107,28 @@ public class AlignmentCalculator extends MetricsCalculator
     }
     resultMetric.setMetricName("AlignmentResults");
     resultMetric.addKeyValue("ReadType", readType.toString());
-    
+
     ResultMetric readInfo = new ResultMetric();
     readInfo.setMetricName("ReadInfo");
     readInfo.addKeyValue("TotalReads", Long.toString(totalReads));
     readInfo.addKeyValue("MappedReads", Long.toString(mappedReads));
-    
+
     readInfo.addKeyValue("UnmappedReads", Long.toString(unmappedReads));
     readInfo.addKeyValue("PercentMapped", getFormattedNumber(percentMapped));
     readInfo.addKeyValue("PercentMismatch", getFormattedNumber(percentMismatch));
     readInfo.addKeyValue("PercentExactMatch", getFormattedNumber(percentExactMatch));
     readInfo.addKeyValue("PercentDuplicate", getFormattedNumber(percentDup));
-    
+
     ResultMetric yieldInfo = new ResultMetric();
     yieldInfo.setMetricName("TotalYield");
     yieldInfo.addKeyValue("TotalBases", Long.toString(totalBases));
     yieldInfo.addKeyValue("ValidBases", Long.toString(totalValidBases));
-    
+    yieldInfo.addKeyValue("EffectiveBases", Long.toString(totalEffectiveBases));
+
     resultMetric.addResultMetric(readInfo);
     resultMetric.addResultMetric(yieldInfo);
   }
-  
+
   /**
    * Private helper method to calculate alignment metrics for the given read
    * @param nextRead
@@ -135,28 +138,30 @@ public class AlignmentCalculator extends MetricsCalculator
   {
 	int numMismatches = 0; // Number of mismatches in current read
 	int readLength = nextRead.getReadLength();
-	
+
     totalReads++;
     totalBases += readLength;
     totalValidBases += countValidBases(nextRead.getReadString());
-    
+
     if(nextRead.getReadUnmappedFlag())
       unmappedReads++;
     else
     {
       mappedReads++;
-      
+
       if(nextRead.getDuplicateReadFlag())
         dupReads++;
-      
+      else
+        totalEffectiveBases += countValidBases(nextRead.getReadString());
+
       // Since the read is mapped, update total number of mapped bases.
-      // This is used to calculate the percentage of mismatches. This is 
+      // This is used to calculate the percentage of mismatches. This is
       // an approximate calculation since we don't look at each base to check
       // if it mapped.
       totalMappedBases += readLength;
-      
+
       numMismatches = mCtr.countMismatches(nextRead);
-    
+
       if(numMismatches == 0)
       {
         totalExactMatches++;
@@ -164,7 +169,7 @@ public class AlignmentCalculator extends MetricsCalculator
       totalMismatches += numMismatches;
     }
   }
-  
+
   /**
    * Count the number of valid bases in a read. Valid bases are the ones without Ns
    * @param baseString - readString
@@ -174,7 +179,7 @@ public class AlignmentCalculator extends MetricsCalculator
   {
     int numValidBases = 0;
     readString = readString.toUpperCase();
-    
+
     for(int i = 0; i < readString.length(); i++)
     {
       if(readString.charAt(i) != 'N')
